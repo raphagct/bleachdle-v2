e4c532d9483968409e6324199f9e3d52dc00085e
Raphaël Guichet <raphael.guichet@etu.unice.fr>
2026-07-03 21:52:21 +0200

    feat: implement quote guessing game logic with random quote selection and character tracking

--- app/(modes)/quotes/page.tsx
+++ app/(modes)/quotes/page.tsx
@@ -9,12 +9,29 @@ import {
     CardFooter,
     CardHeader
 } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CharacterSearch from "@/components/shared/CharacterSearch";
+import { quotes, type Quote } from "@/lib/quotes.data"
+import { characters, type Character } from "@/lib/characters.data";
+import Image from "next/image";
 
 export default function QuotesModePage() {
     const [attemptFirstHint, setAttemptFirstHint] = useState(4);
     const [attemptSecondHint, setAttemptSecondHint] = useState(7);
+    const [randomQuote, setRandomQuote] = useState<Quote | null>(null)
+    const [charactersPlayed, setCharactersPlayed] = useState<Character[]>([])
+    const characterTries = charactersPlayed.slice().reverse()
+
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * quotes.length)
+        setRandomQuote(quotes[randomIndex])
+    }, []);
+
+    const handleCharacterSelected = (character: Character) => {
+        setCharactersPlayed((prev) => [...prev, character])
+    }
+
 
     return <div className="px-4">
         <Card className="max-w-lg mx-auto">
@@ -24,9 +41,7 @@ export default function QuotesModePage() {
             <CardContent>
                 <blockquote className="mb-4 border-l-4 border-primary pl-6 py-2">
                     <p className="text-xl font-serif italic text-foreground leading-relaxed">
-                        "C'est quoi un cœur ? Si je t'ouvre la poitrine,
-                        est-ce que je le verrai à l'intérieur ?
-                        Si je te fracasse le crâne, est-ce qu'il sera là ?"
+                        "{randomQuote?.quote}"
                     </p>
                 </blockquote>
                 <Hints
@@ -43,6 +58,26 @@ export default function QuotesModePage() {
                 <p>Les données vont jusqu'à la fin de l'arc Thousand-Year Blood War</p>
             </CardFooter>
         </Card>
+        <CharacterSearch
+            charactersPlayed={charactersPlayed}
+            onSelect={handleCharacterSelected}
+            allowedCharacterIds={quotes.map((quotes) => quotes.character_id)} />
+        {charactersPlayed.length > 0 && characterTries.map((character) => {
+            const isWin = character.id === randomQuote?.character_id;
+            return <div key={character.id}
+                className="max-w-lg mx-auto mt-4 p-4 gap-4 border rounded-lg flex items-center justify-center font-semibold text-white"
+                style={{ background: isWin ? "green" : "red" }}>
+                <Image
+                    src={character.image_url}
+                    alt={character.name + " icon"}
+                    width={60}
+                    height={60} />
+                <span>{character.name}</span>
+            </div>
+        })
+
+        }
+
 
     </div>
 }
---
+++ lib/quotes.data.ts
@@ -0,0 +1,108 @@
+export type Quote = {
+    id: number,
+    quote: string,
+    character_id: number
+}
+
+export const quotes: Quote[] = [
+    {
+        id: 1,
+        quote: "C'est quoi un cœur ? Si je t'ouvre la poitrine, est-ce que je le verrai à l'intérieur ? Si je te fracasse le crâne, est-ce qu'il sera là ?",
+        character_id: 14
+    },
+    {
+        id: 2,
+        quote: "L'admiration est le sentiment qui est le plus éloigné de la compréhension.",
+        character_id: 7
+    },
+    {
+        id: 3,
+        quote: "Depuis le début, personne n'est au sommet du ciel. Ni toi, ni moi, ni même les Dieux. Mais le vacance insupportable du trône céleste est finie. Dès à présent, c'est moi qui m'y assieds.",
+        character_id: 7
+    },
+    {
+        id: 4,
+        quote: "Je ne me bats pas parce que j'ai l'espoir de gagner. Je me bats parce que je dois gagner !",
+        character_id: 1
+    },
+    {
+        id: 5,
+        quote: "La différence entre nos forces n'est pas une question de niveau. Elle est aussi profonde que le fossé qui sépare la terre du ciel.",
+        character_id: 4
+    },
+    {
+        id: 6,
+        quote: "Quand je combats quelqu'un de fort, je suis tellement excité que je ne peux pas m'empêcher de sourire.",
+        character_id: 6
+    },
+    {
+        id: 7,
+        quote: "Un guerrier qui ne craint pas sa propre épée n'est pas digne de la tenir dans ses mains.",
+        character_id: 11
+    },
+    {
+        id: 8,
+        quote: "Dès l'instant où la guerre éclate, les deux camps ont tort. Il n'y a pas de justice dans la guerre.",
+        character_id: 17
+    },
+    {
+        id: 9,
+        quote: "Ne m'oublie jamais, Shinigami ! Je suis celui qui t'a amené au seuil de la mort !",
+        character_id: 13
+    },
+    {
+        id: 10,
+        quote: "Même si personne ne croit en toi, tu dois être le seul à croire en toi-même.",
+        character_id: 2
+    },
+    {
+        id: 11,
+        quote: "Si tu devais te transformer en serpent demain et commencer à dévorer des humains, et que de cette même bouche tu me disais que tu m'aimes... serais-je encore capable de te dire que je t'aime aujourd'hui ?",
+        character_id: 8
+    },
+    {
+        id: 12,
+        quote: "La perfection est une absurdité. S'il existe quelque chose de parfait, alors il n'y a plus de place pour l'amélioration, plus de place pour l'imagination !",
+        character_id: 20
+    },
+    {
+        id: 13,
+        quote: "Le conflit n'est pas un péché. C'est le moteur de toute évolution dans ce monde.",
+        character_id: 19
+    },
+    {
+        id: 14,
+        quote: "Un homme qui ne peut pas défendre l'honneur d'une femme n'est rien de plus qu'un chien !",
+        character_id: 3
+    },
+    {
+        id: 15,
+        quote: "Si tu oses faire couler le sang d'Hinamori une seule fois de plus... je te tuerai de mes propres mains !",
+        character_id: 5
+    },
+    {
+        id: 16,
+        quote: "Je ne me bats pas pour les Shinigamis. Je me bats sur mon honneur en tant que Quincy !",
+        character_id: 9
+    },
+    {
+        id: 17,
+        quote: "J'aurais aimé avoir cinq vies ! Je pourrais naître dans cinq villes différentes, manger cinq fois plus de bonnes choses... et je tomberais amoureuse de la même personne cinq fois.",
+        character_id: 10
+    },
+    {
+        id: 18,
+        quote: "La justice sans force est impuissante, mais la force sans justice n'est que de la tyrannie. Je suis la voie de la moindre effusion de sang.",
+        character_id: 18
+    },
+    {
+        id: 19,
+        quote: "Dans ce monde, il y a des choses qu'on ne peut pas comprendre simplement parce qu'on ouvre les yeux.",
+        character_id: 15
+    },
+    {
+        id: 20,
+        quote: "Ce ne sont pas les armes qui font la force d'un guerrier, c'est la détermination qui anime son âme.",
+        character_id: 12
+    }
+];
